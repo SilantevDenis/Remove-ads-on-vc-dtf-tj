2b743050b35bee91b6ee57d397a039bb448374e7
Denis Silantev <d.silantjev2012@gmail.com>
2020-09-18 00:27:10 +0300

    Update Remove ads on vc dtf tj .user.js

--- Remove ads on vc dtf tj .user.js
+++ Remove ads on vc dtf tj .user.js
@@ -9,7 +9,7 @@
 // @match        https://journal.tinkoff.ru/*
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.5.1.slim.js
-// @require      https://code.jquery.com/jquery-3.5.1.slim.js
+// @require      https://raw.githubusercontent.com/SilantevDenis/Remove-ads-on-vc-dtf-tj/master/scripts.js
 // ==/UserScript==
 
 (function () {
